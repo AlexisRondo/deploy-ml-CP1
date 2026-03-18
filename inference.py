cf2107a3b5d8f1a0e9c432cc4273887675e47df7
AlexisRondo <alan101rqr@gmail.com>
2026-03-18 19:18:26 -0300

    Refactor inference.py for model and port handling
    Updated model variable name and added dynamic port configuration.

--- inference.py
+++ inference.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
+import os
 
 with open("modelo.pkl", "rb") as f:
-    model = pickle.load(f)
-    
+    modelo = pickle.load(f)
+
 app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    dados  = request.json
+    dados = request.json
     df = pd.DataFrame(dados)
-    pred = model.predict(df)
+    pred = modelo.predict(df)
     return jsonify({"predicao": pred.tolist()})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
